0588a06dfbb1a0d7c93c4d9bf448cd07c259d919
IDO <susususu07@naver.com>
2024-01-11 03:39:00 +0900

    Add files via upload

---
+++ variableTest.py
@@ -0,0 +1,20 @@
+print(5000)
+print(5000 * 2)
+print(5000 + 3500)
+print(5000 * 2 + 3500 * 1)
+
+
+# 변수 사용 목적 
+# 1. 해당 값이 무엇을 의미하는 값인지
+#   쉽게 알 수 있다
+# 2. 값이 수정될 때, 
+#   번거로움을 줄일 수 있다
+
+burger_price = 5000
+fries_price = 3500
+
+print(burger_price)
+print(burger_price * 2)
+print(burger_price + fries_price)
+print(burger_price * 2 + fries_price * 1)
+
